523b42c795f24545e2aea5f3ce6a116cd2024ec4
David <91037972+Davidevlops@users.noreply.github.com>
2022-06-24 12:01:48 +0100

    Add files via upload

---
+++ Javascript Calculator/calc.js
@@ -0,0 +1,25 @@
+firstNumber = Number(prompt("Enter your Number: "));
+secondNumber = Number(prompt("Enter second Number: "));
+const addition = firstNumber + secondNumber;
+const subtration = firstNumber - secondNumber;
+const multiplication = firstNumber * secondNumber;
+const division = firstNumber / secondNumber;
+const modulo = firstNumber % secondNumber;
+
+document.write(
+  "addition = ",
+  addition,
+  "<br>",
+  "subtration = ",
+  subtration,
+  "<br>",
+  "multiplication = ",
+  multiplication,
+  "<br>",
+  "division = ",
+  division,
+  "<br>",
+  "modulo = ",
+  modulo,
+  "<br>"
+);
